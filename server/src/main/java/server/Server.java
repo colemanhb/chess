ee057293172123f48973b71ccb12a0ff7be343bb
colemanhb <colemanhallbrown@gmail.com>
2025-10-19 18:58:11 -0600

    Server class functions completed

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,34 +3,69 @@ package server;
 import com.google.gson.Gson;
 import dataaccess.DataAccess;
 import dataaccess.MemoryDataAccess;
-import model.RegisterRequest;
-import model.UserData;
+import model.*;
 import io.javalin.*;
 import io.javalin.http.Context;
+import org.jetbrains.annotations.NotNull;
 import service.Service;
 import service.ServiceException;
-
-import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class Server {
 
     private final Javalin httpHandler;
-    private Service service;
-    private DataAccess dataAccess;
+    private final Service service;
 
     public Server() {
-        dataAccess = new MemoryDataAccess();
+        DataAccess dataAccess = new MemoryDataAccess();
         service = new Service(dataAccess);
         httpHandler = Javalin.create(config -> config.staticFiles.add("web"))
         // Register your endpoints and exception handlers here.
                 .post("/user", this::register)
-                /*.post("/session/{username,password}", this::login)
-                .delete("/session/{authToken}", this::logout)
-                .get("/game/{authToken", this::listGames)
-                .post("/game/authToken/{gameName}", this::createGame)
-                .put("/game/authToken/{playerColor,gameID}", this::joinGame)*/
+                .post("/session", this::login)
+                .delete("/session", this::logout)
+                .get("/game", this::listGames)
+                .post("/game", this::createGame)
+                .put("/game", this::joinGame)
                 .delete("/db", this::clear)
                 .exception(ServiceException.class, this::exceptionHandler);
+    }
+
+    private void joinGame(@NotNull Context ctx) throws Exception {
+        var serializer = new Gson();
+        String jsonRequest = ctx.body();
+        var request = serializer.fromJson(jsonRequest, JoinGameRequest.class);
+        //call to the service
+        service.joinGame(request);
+
+    }
+
+    private void createGame(@NotNull Context ctx) throws Exception {
+        var serializer = new Gson();
+        String jsonRequest = ctx.body();
+        var request = serializer.fromJson(jsonRequest, CreateGameRequest.class);
+        //call to the service
+        var res = service.createGame(request);
+        ctx.result(serializer.toJson(res));
+
+    }
+
+    private void listGames(@NotNull Context ctx) throws Exception {
+        var serializer = new Gson();
+        String jsonRequest = ctx.body();
+        var request = serializer.fromJson(jsonRequest, AuthorizationRequest.class);
+        //call to the service
+        var res = service.listGames(request);
+        ctx.result(serializer.toJson(res));
+
+    }
+
+    private void logout(@NotNull Context ctx) throws Exception {
+        var serializer = new Gson();
+        String jsonRequest = ctx.body();
+        var request = serializer.fromJson(jsonRequest, AuthorizationRequest.class);
+        //call to the service
+        service.logout(request);
+
     }
 
     private void register(Context ctx) throws Exception{
@@ -39,6 +74,15 @@ public class Server {
         var request = serializer.fromJson(jsonRequest, RegisterRequest.class);
         //call to the service and register
         var res = service.register(request);
+        ctx.result(serializer.toJson(res));
+    }
+
+    private void login(Context ctx) throws Exception {
+        var serializer = new Gson();
+        String jsonRequest = ctx.body();
+        var request = serializer.fromJson(jsonRequest, LoginRequest.class);
+        //call to the service
+        var res = service.login(request);
         ctx.result(serializer.toJson(res));
     }
 
