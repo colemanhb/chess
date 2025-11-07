832a3db3a4950ce2138890ffea057b9c1494f432
colemanhb <colemanhallbrown@gmail.com>
2025-11-07 15:28:24 -0700

    Server Facade Started

--- server/src/main/java/service/ServiceException.java
+++ server/src/main/java/service/ServiceException.java
@@ -2,9 +2,27 @@ package service;
 
 import com.google.gson.Gson;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ServiceException extends Exception {
+
+    public static ServiceException fromJson(String json) {
+        var map = new Gson().fromJson(json, HashMap.class);
+        var status = Code.valueOf(map.get("status").toString());
+        String message = map.get("message").toString();
+        return new ServiceException(message, status);
+    }
+
+    public static Code fromHttpStatusCode(int httpStatusCode) {
+        return switch (httpStatusCode) {
+            case 500, 402 -> Code.ServerError;
+            case 400 -> Code.BadRequestError;
+            case 403 -> Code.AlreadyTakenError;
+            case 401 -> Code.NotFoundError;
+            default -> throw new IllegalStateException("Unexpected value: " + httpStatusCode);
+        };
+    }
 
     public enum Code {
         AlreadyTakenError,
---
+++ shared/src/main/java/server/ServerFacade.java
@@ -0,0 +1,74 @@
+package server;
+
+import com.google.gson.Gson;
+import model.LoginResult;
+import model.RegisterRequest;
+import service.Service;
+import service.ServiceException;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ServerFacade {
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public LoginResult register(RegisterRequest registerRequest) throws ServiceException{
+        var request = buildRequest("POST", "/user", registerRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, LoginResult.class);
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private HttpRequest.BodyPublisher makeRequestBody(Object request) {
+        if(request != null) {
+            return HttpRequest.BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return HttpRequest.BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws ServiceException {
+        try {
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new ServiceException(ex.getMessage(), ServiceException.Code.ServerError);
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ServiceException {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if(body != null) {
+                throw ServiceException.fromJson(body);
+            }
+
+            throw new ServiceException("other failure" + status, ServiceException.fromHttpStatusCode(status));
+        }
+        if(responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
